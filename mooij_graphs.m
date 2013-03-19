e01197b3a6b594d190b247f2b2da9e8bcfcd3ea6
Kui Tang <kuitang@gmail.com>
2013-03-18 22:02:44 -0400

    fixed nNodes error

--- mooij_graphs.m
+++ mooij_graphs.m
@@ -8,8 +8,10 @@
 %% Setup
 path_to_dai = '../libDAI-0.3.1/matlab';
 addpath(path_to_dai);
-nTrials = 10;
+nTrials = 100;
 
+nNodes = 4;
+        
 % trueLogZ
 epsilon = 0.1;
 
@@ -41,21 +43,20 @@ mooijOpts = struct('useMooij', true);
 
 problems = cell(1, nTrials);
 
+successVec(nTrials) = false;
+
 fn = ['results ' num2str(nNodes) ' nodes ' datestr(now, 0);];
 %% Loop
 for t = 1:nTrials
     
-    %% Make problem
-    N = 4;
-        
     % Draw (t,j) from the triangle (-1,2), (1,2), (0,1)
     j  = unifrnd(1, 2)                          % Uniform coupling strength            
     dt = j - 1;
     th = unifrnd(-dt, dt)
     
-    J = j * ones(N,N);
+    J = j * ones(nNodes,nNodes);
     J = J - diag(diag(J));
-    eta = th * ones(N,1);  
+    eta = th * ones(nNodes,1);  
 
     % convert from (-1,+1) to (0,1) convention
     theta = 2 * eta - 2 * sum(J,2); % zeta is called theta in the paper
@@ -70,8 +71,14 @@ for t = 1:nTrials
     disp(['LBP Finished']);
     
     %% Run the time-killer
-    [mooijlogZ, oneMarg(:,t,2), twoMarg, misc] = BetheApprox_opt_mex(theta, W, epsilon, mooijOpts);            
-    fprintf(1, 'logZ: Mooij: %g; True: %g; LBP: %g\n', mooijlogZ, trueLogZ, lbpLogZ);       
+    try
+      [mooijlogZ, oneMarg(:,t,2), twoMarg, misc] = BetheApprox_opt_mex(theta, W, epsilon, mooijOpts);            
+      fprintf(1, 'logZ: Mooij: %g; True: %g; LBP: %g\n', mooijlogZ, trueLogZ, lbpLogZ);       
+      successVec(t) = true;
+    catch err
+      disp(err)
+      successVec(t) = false;
+    end
     
     % [logZ, oneMarg, twoMarg, misc] = BetheApprox_opt_mex(theta, W, epsilon, opts);   
     % disp(['Bethe Finished']);
@@ -83,7 +90,7 @@ for t = 1:nTrials
 %     totalGap = sum(gaps(:));
 %     nIntervals = totalGap / isz
 % 
-%     [Am, Bm] = bpbound(N, theta, W, 1000)
+%     [Am, Bm] = bpbound(nNodes, theta, W, 1000)
 %     [iszm, kappa, theoryboundm] = getIntervalSz(Am, Bm, W, 1e-2)
 %     mGaps = 1 - Bm - Am;
 %     totalMGaps = sum(mGaps(:));
